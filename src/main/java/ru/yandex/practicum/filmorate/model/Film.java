cdc79e94348b52af92366b48cbc9890e1c7d633e
GrimJak <a.gagarskiy@yandex.kz>
2024-12-16 18:16:03 +0300

    ТЗ-10 (поиск ошибки)

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,30 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+import ru.yandex.practicum.filmorate.validators.Add;
+import ru.yandex.practicum.filmorate.validators.Update;
+
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
 public class Film {
+    @NotNull(groups = {Update.class}, message = "Id отсутствует")
+    Long id;
+    @NotNull(groups = {Add.class, Update.class}, message = "Name can not be null")
+    @NotBlank(groups = {Add.class, Update.class}, message = "Name can not be blank")
+    String name;
+    @Size(groups = {Add.class, Update.class}, max = 200, message = "Max size is 200")
+    String description;
+    LocalDate releaseDate;
+    @Positive(groups = {Add.class, Update.class}, message = "duration must be positive")
+    Integer duration;
+
+    @AssertTrue(groups = {Add.class, Update.class}, message = "Release date invalid")
+    public boolean isValideReleaseDate() {
+        return releaseDate.isAfter(LocalDate.of(1895, 12, 28));
+    }
 }
